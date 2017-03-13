bd752bc62687e66a050ca288bd7ac50783e947f1
Hilmi Emre Bayat <hilmiemrebayat@users.noreply.github.com>
2017-03-13 23:30:07 +0100

    Update SQLInstall.ps1

--- scripts/SQLInstall.ps1
+++ scripts/SQLInstall.ps1
@@ -1,5 +1,5 @@
 Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?LinkID=799012" -OutFile "C:\Users\vagrant\Desktop\setup.exe"
-Invoke-WebRequest -Uri "https://goo.gl/0YZqxF" -OutFile "C:\Users\Vagrant\Desktop\ConfigurationFile.ini"
-$configfile = "C:\Users\Vagrant\Desktop\ConfigurationFile.ini"
+Invoke-WebRequest -Uri "https://goo.gl/9994dA" -OutFile "C:\Users\Vagrant\Desktop\SQLConfigurationFile.ini"
+$configfile = "C:\Users\Vagrant\Desktop\SQLConfigurationFile.ini"
 $command = "C:\Users\Vagrant\Desktop\setup.exe /ConfigurationFile=$($configfile)"
 Invoke-Expression -Command $command 
