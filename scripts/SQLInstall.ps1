0db43dd7371acd7915115bec7d93b43c0d0445d2
Hilmi Emre Bayat <hilmiemrebayat@users.noreply.github.com>
2017-03-13 22:23:12 +0100

    Update SQLInstall.ps1

--- scripts/SQLInstall.ps1
+++ scripts/SQLInstall.ps1
@@ -1,3 +1,5 @@
-$configfile = "ConfigurationFile.ini"
-$command = "C:\HashiCorp\Vagrant\vagrant\scripts\resources\setup.exe /ConfigurationFile=$($configfile)"
+Invoke-Request -Uri "https://www.microsoft.com/en-us/download/confirmation.aspx?id=42299" -OutFile "C:\Users\Vagrant\Destkop\sql.exe"
+Invoke-Request -Uri "https://goo.gl/0YZqxF" -OutFile "C:\Users\Vagrant\Destkop\sql.exe"
+$configfile = "C:\Users\Vagrant\Destkop\ConfigurationFile.ini"
+$command = "C:\Users\Vagrant\Destkop\sql.exe /ConfigurationFile=$($configfile)"
 Invoke-Expression -Command $command 
